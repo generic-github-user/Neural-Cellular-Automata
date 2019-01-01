38c67c9814ddfa040cd78843bdb6d0e452312e44
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-01 14:21:19 -0500

    Add value range mapping function
    https://stackoverflow.com/a/23202637

--- src/script.js
+++ src/script.js
@@ -3,6 +3,12 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Map one range of numbers to another, given an input value and the two ranges
+// https://stackoverflow.com/a/23202637
+const map = function(num, in_min, in_max, out_min, out_max) {
+      return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 var cells = [];
 for (var i = 0; i < 50; i++) {
       var row = [];
